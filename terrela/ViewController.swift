2461f596e692a39f335d4634a5c28b254e3614dc
sicaramacuya <eric.morales-rodriguez@students.makeschool.com>
2021-07-12 22:15:14 -0400

    removes storyboard

--- terrela/ViewController.swift
+++ terrela/ViewController.swift
@@ -8,12 +8,34 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    let testingView: UIView = {
+        let myView = UIView(frame: .zero)
+        myView.translatesAutoresizingMaskIntoConstraints = false
+        myView.backgroundColor = .systemPink
+        
+        return myView
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        setupMyView()
     }
-
+    
+    func setupMyView() {
+        view.addSubview(testingView)
+        
+        testingView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+        testingView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+        testingView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+        testingView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+        
+        testingView.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 1.0).isActive = true
+        testingView.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 1.0).isActive = true
+        
+    }
 
 }
 
