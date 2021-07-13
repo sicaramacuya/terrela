5e97f839fb208af96799af1345cfb296664c57c4
sicaramacuya <eric.morales-rodriguez@students.makeschool.com>
2021-07-13 09:27:58 -0400

    add testing ground for views

---
+++ terrela/Testing Ground/Controller/TestingVC.swift
@@ -0,0 +1,20 @@
+//
+//  TestingVC.swift
+//  terrela
+//
+//  Created by Eric Morales on 7/13/21.
+//
+
+import UIKit
+
+class TestingVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+        view.backgroundColor = .darkGray
+        TitleStack(homeView: self.view)
+    }
+}
