0a41c40f899b1af9cf865f9bf06ac79437627296
sicaramacuya <eric.morales-rodriguez@students.makeschool.com>
2021-07-14 19:00:31 -0400

    adding readme

---
+++ terrela/Testing Ground/View/TestingCategoryCell.swift
@@ -0,0 +1,12 @@
+//
+//  TestingCategoryCell.swift
+//  terrela
+//
+//  Created by Eric Morales on 7/13/21.
+//
+
+import UIKit
+
+class TestingCategoryCell: UICollectionViewCell {
+    
+}
---
+++ terrela/Testing Ground/View/TestingCategorySection.swift
@@ -0,0 +1,8 @@
+//
+//  TestingCategorySection.swift
+//  terrela
+//
+//  Created by Eric Morales on 7/13/21.
+//
+
+import Foundation
