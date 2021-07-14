f7630fd1deb56eedf1fb0f68d95f0cea2eb6eedf
sicaramacuya <eric.morales-rodriguez@students.makeschool.com>
2021-07-14 02:42:29 -0400

    refactor viewController to display titleStack and collectionView

--- terrela/Home/Controller/HomeVC.swift
+++ terrela/Home/Controller/HomeVC.swift
@@ -9,20 +9,81 @@ import UIKit
 
 class HomeVC: UIViewController {
     // MARK: Properties
-    let menuButton: UIButton = {return UIButton()}()
-    
-    let bottomStack: UIStackView = {return UIStackView()}()
-    let collectionView: UICollectionView! = nil // will have title section and cells
+    var collectionView: UICollectionView!
+    lazy var sections: [Section] = [
+        TitleSection(title: "What do you want to learn about?"),
+        CategorySection()
+    ]
+    lazy var collectionViewLayout: UICollectionViewLayout = {
+        var sections = self.sections
+        let layout = UICollectionViewCompositionalLayout { (sectionIndex, environment) -> NSCollectionLayoutSection? in
+            
+            return sections[sectionIndex].layoutSection()
+        }
+        return layout
+    }()
     
     // MARK: VC Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
         
-        view.backgroundColor = .systemGray6
+        self.title = "Home"
+        self.view.backgroundColor = .systemGray6
         TitleStack(homeView: self.view)
+        setupCollectionView()
     }
     
     // MARK: Methods
+    func setupCollectionView() {
+        
+        // instanciate
+        collectionView = UICollectionView(frame: .zero, collectionViewLayout: collectionViewLayout)
+        
+        // properties
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        collectionView.backgroundColor = .systemGray6
+        
+        // setting delegate and data source
+        collectionView.dataSource = self
+        collectionView.delegate = self
+        
+        // registering
+        collectionView.register(TitleCell.self, forCellWithReuseIdentifier: TitleCell.identifier)
+        collectionView.register(TestingCategoryCell.self, forCellWithReuseIdentifier: TestingCategoryCell.identifier)
+
+        // adding to the view
+        self.view.addSubview(collectionView)
+        
+        // constraints
+        collectionView.heightAnchor.constraint(equalTo: self.view.heightAnchor, constant: 0.5).isActive = true
+        collectionView.widthAnchor.constraint(equalTo: self.view.widthAnchor).isActive = true
+        collectionView.topAnchor.constraint(equalTo: self.view.bottomAnchor, constant: -250).isActive = true
+        
+        // reloads all data in collectionView
+        collectionView.reloadData()
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        collectionView.reloadData()
+    }
+    
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        collectionView.reloadData()
+    }
+}
+
+extension HomeVC: UICollectionViewDataSource, UICollectionViewDelegate {
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return sections.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return sections[section].numberOfItems
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        return sections[indexPath.section].configureCell(collectionView: collectionView, indexPath: indexPath)
+    }
 }
 
