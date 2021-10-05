f964a1876c69b68db03b86d91064625a43936b11
sicaramacuya <eric.morales@protonmail.com>
2021-10-05 11:02:54 -0400

    Clean up ignored files

--- terrela/Testing Ground/Controller/TestingVC.swift
+++ terrela/Testing Ground/Controller/TestingVC.swift
@@ -8,13 +8,80 @@
 import UIKit
 
 class TestingVC: UIViewController {
-
+    // MARK: Properties
+    var collectionView: UICollectionView!
+    lazy var sections: [Section] = [
+        TitleSection(title: "What do you want to learn about?"),
+        TestingCategorySection()
+    ]
+    lazy var collectionViewLayout: UICollectionViewLayout = {
+        var sections = self.sections
+        let layout = UICollectionViewCompositionalLayout { (sectionIndex, environment) -> NSCollectionLayoutSection? in
+            
+            return sections[sectionIndex].layoutSection()
+        }
+        return layout
+    }()
+    
+    // MARK: VC Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
         
-        view.backgroundColor = .darkGray
-        TitleStack(homeView: self.view)
+        self.title = "Home"
+        self.view.backgroundColor = .white
+        setupCollectionView()
+    }
+    
+    // MARK: Methods
+    func setupCollectionView() {
+        
+        // instanciate
+        collectionView = UICollectionView(frame: .zero, collectionViewLayout: collectionViewLayout)
+        
+        // properties
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        collectionView.backgroundColor = .systemRed
+        
+        // setting delegate and data source
+        collectionView.dataSource = self
+        collectionView.delegate = self
+        
+        // registering
+        collectionView.register(TitleCell.self, forCellWithReuseIdentifier: TitleCell.identifier)
+        collectionView.register(TestingCategoryCell.self, forCellWithReuseIdentifier: TestingCategoryCell.identifier)
+
+        // adding to the view
+        self.view.addSubview(collectionView)
+        
+        // constraints
+        collectionView.heightAnchor.constraint(equalTo: self.view.heightAnchor, constant: 0.5).isActive = true
+        collectionView.widthAnchor.constraint(equalTo: self.view.widthAnchor).isActive = true
+        collectionView.topAnchor.constraint(equalTo: self.view.topAnchor, constant: 500).isActive = true
+        
+        // reloads all data in collectionView
+        collectionView.reloadData()
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        collectionView.reloadData()
+    }
+    
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        collectionView.reloadData()
+    }
+}
+
+extension TestingVC: UICollectionViewDataSource, UICollectionViewDelegate {
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return sections.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return sections[section].numberOfItems
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        return sections[indexPath.section].configureCell(collectionView: collectionView, indexPath: indexPath)
     }
 }
--- terrela/Testing Ground/View/TestingCategorySection.swift
+++ terrela/Testing Ground/View/TestingCategorySection.swift
@@ -5,4 +5,34 @@
 //  Created by Eric Morales on 7/13/21.
 //
 
-import Foundation
+import UIKit
+
+struct TestingCategorySection: Section {
+    // MARK: Properties
+    let numberOfItems = 3
+    
+    // MARK: Methods
+    func layoutSection() -> NSCollectionLayoutSection? {
+        // item
+        let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: .fractionalHeight(1.0))
+        let item = NSCollectionLayoutItem(layoutSize: itemSize)
+        item.contentInsets = NSDirectionalEdgeInsets(top: 0, leading: 0, bottom: 10, trailing: 0)
+        
+        
+        // group
+        let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(0.95), heightDimension: .fractionalHeight(0.12))
+        let group = NSCollectionLayoutGroup.vertical(layoutSize: groupSize, subitem: item, count: 1)
+        
+        // section
+        let section = NSCollectionLayoutSection(group: group)
+        section.orthogonalScrollingBehavior = .groupPaging
+        section.contentInsets = NSDirectionalEdgeInsets(top: 0, leading: 10, bottom: 0, trailing: 0)
+        
+        return section
+    }
+    
+    func configureCell(collectionView: UICollectionView, indexPath: IndexPath) -> UICollectionViewCell {
+        collectionView.dequeueReusableCell(withReuseIdentifier: String(describing: CategoryCell.self), for: indexPath)
+    }
+}
+
